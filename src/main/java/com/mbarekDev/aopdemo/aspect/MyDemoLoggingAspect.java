3e7aee0d05c51ff865d09530f2b7db8cda7e4c51
Mbarek Benraiss <81750067+malika-1989@users.noreply.github.com>
2025-07-10 07:48:19 +0200

    AOP-Pointcut Declaration

--- src/main/java/com/mbarekDev/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/mbarekDev/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -3,6 +3,7 @@ package com.mbarekDev.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -16,9 +17,19 @@ public class MyDemoLoggingAspect {
     //@Before("execution(public void updateAccount())")
 
     //@Before("execution(public void com.mbarekDev.aopdemo.dao.addAccount())")
-// .. -> means match on any number of argumennts () use param wildcards
-    @Before("execution(* com.mbarekDev.aopdemo.dao.*.*(..))")
-    public void beforeAddAccountAdvice(){
+    //.. -> means match on any number of argumennts () use param wildcards
+    // step1, Create pointcut declaration
+    @Before("forDaoPackage()")
+    public void beforeAddAccountAdvice() {
         System.out.println("\n ----> Executing @Before advice on addAccount() ");
     }
+
+    // step1, Apply pointcut declaration to advice
+    @Pointcut("execution(* com.mbarekDev.aopdemo.dao.*.*(..))")
+    private void forDaoPackage() {
+
+
+    }
+
+
 }
